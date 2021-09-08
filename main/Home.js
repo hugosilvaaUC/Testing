472b323152b972576a31c80541d1ea6cc7a2b434
hugosilvaaUC <90317290+hugosilvaaUC@users.noreply.github.com>
2021-09-08 11:19:17 +0100

    dada

--- main/Home.js
+++ main/Home.js
@@ -1,1 +1,28 @@
-console.log("working");
+import Usercentrics, { UI_LAYER, UI_VARIANT } from '@usercentrics/cmp-browser-sdk';
+
+const UC = new Usercentrics('YOUR_USERCENTRICS_SETTINGS_ID');
+
+UC.init().then((initialUIValues) => {
+  // getSettings() returns all Usercentrics settings you need for your custom solution
+  const settings = UC.getSettings();
+  // getCategories() returns all categories' and data processing services' information
+  const categories = UC.getCategories();
+
+  if (initialUIValues.variant === UI_VARIANT.DEFAULT) {
+    switch (initialUIValues.initialLayer) {
+      case UI_LAYER.FIRST_LAYER:
+        // Show first layer (i.e. privacy banner)
+        console.log("teste");
+        return;
+      case UI_LAYER.PRIVACY_BUTTON:
+        // Show privacy button
+        console.log(":DDD");
+        return;
+      case UI_LAYER.NONE:
+      default:
+          console.log("Teste");
+        // Show nothing
+        return;
+    }
+  }
+});
